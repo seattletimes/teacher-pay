4fb5d395376ae9d0eb42ce1409c6f050fb160328
Audrey Carlsen <acarlsen@seattletimes.com>
2015-04-30 11:53:56 -0700

    calculates total max salary for comparison

--- src/js/main.js
+++ src/js/main.js
@@ -5,18 +5,29 @@ require("angular");
 
 var app = angular.module("teacher-pay", []);
 
-var seattle = salaryData.filter(function(district) { return district.title == "Seattle" });
 var barMeasures = ["yr1-ba", "yr8-ba90", "yr15-ma45"];
+var max = 0;
+
+salaryData.map(function(district) {
+  barMeasures.forEach(function(point) {
+    if (district["base-"+point] && district["tri-"+point]) {
+      district["base-"+point] = district["base-"+point] * 1;
+      district["tri-"+point] = district["tri-"+point] * 1;
+      district[point+"-sum"] = (district["base-"+point] + district["tri-"+point]);
+
+      if (district[point+"-sum"] > max) { max = district[point+"-sum"] }
+    }
+  })
+  return district;
+});
+
+var seattle = salaryData.filter(function(district) { return district.title == "Seattle" })[0];
 
 app.controller("SalaryController", ["$scope", function($scope) {
   $scope.districts = salaryData.map(function(district) {
     barMeasures.forEach(function(point) {
-      if (district["base-"+point] && district["tri-"+point]) {
-        district["base-"+point] = district["base-"+point] * 1;
-        district["tri-"+point] = district["tri-"+point] * 1;
-        var sum = (district["base-"+point] + district["tri-"+point]);
-
-        district[point+"-bar"] = sum / 1000;
+      if (district[point+"-sum"]) {
+        district[point+"-bar"] = district[point+"-sum"] / max * 100;
       }
     })
     return district;
