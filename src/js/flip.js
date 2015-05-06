4d7fd338ffcd9c066b05607a40bd1a5416110493
Thomas Wilburn <news@thomaswilburn.net>
2015-05-06 14:35:25 -0700

    Oh god, directives gone mad

---
+++ src/js/flip.js
@@ -0,0 +1,48 @@
+require("angular");
+
+var transform = "transform" in document.body.style ? "transform" : "webkitTransform";
+
+module.exports = function(app) {
+  app.directive("flipOn", function() {
+    return {
+      restrict: "A",
+      link: function(scope, el, attr) {
+        el = el[0];
+        var first = el.getBoundingClientRect();
+        scope.$watch("$index", function() {
+          var last = el.getBoundingClientRect();
+          var dx = first.left - last.left;
+          var dy = first.top - last.top;
+          var style = `translate(${dx}px, ${dy}px)`
+          el.style[transform] = style;
+          //read position
+          var _ = el.offsetWidth;
+          el.classList.add("flipping");
+          el.style.transform = `translate(0)`;
+          first = last;
+          setTimeout(() => el.classList.remove("flipping"), 1000);
+        });
+      }
+    }
+  });
+
+  var forEach = function(obj, f) {
+    for (var key in obj) f(obj[key], key);
+  }
+
+  app.directive("delayedStyle", function() {
+    return {
+      restrict: "A",
+      link: function(scope, element, attr) {
+        scope.$watch(attr.delayedStyle, function ngStyleWatchAction(newStyles, oldStyles) {
+          setTimeout(function() {
+            if (oldStyles && (newStyles !== oldStyles)) {
+              forEach(oldStyles, function(val, style) { element.css(style, '');});
+            }
+            if (newStyles) element.css(newStyles);
+          }, 10);
+        }, true);
+      }
+    }
+  })
+}
--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,8 @@ require("angular");
 
 var app = angular.module("teacher-pay", []);
 
+require("./flip")(app); //mix in our directive
+
 var barMeasures = ["low", "middle", "high"];
 var max = 0;
 
@@ -32,26 +34,31 @@ salaryData.map(function(district) {
 
 var seattle = salaryData.filter(function(district) { return district.title == "Seattle" })[0];
 
-app.controller("SalaryController", ["$scope", function($scope) {
-  $scope.payPoint = "middle";
+app.controller("SalaryController", ["$scope", "$timeout", function($scope, $timeout) {
+  $scope.payPoint = "high";
   $scope.featuredDistrict = seattle;
   $scope.dialogue = false;
+  $scope.districts = salaryData;
+  $scope.ordering = "sum-high";
 
-  $scope.sortDistricts = function() {
-    $scope.districts = salaryData.sort(function(a,b) {
+  // $scope.sortDistricts = function() {
+    $scope.districts.sort(function(a,b) {
       return (b["sum-"+$scope.payPoint] * 1) - (a["sum-"+$scope.payPoint] * 1);
     });
-  };
+  // };
 
   $scope.setDistrict = function(district) {
     $scope.featuredDistrict = district;
     $scope.dialogue = true;
   };
 
+  $scope.ordering = function(item) {
+    return -item["sum-" + $scope.payPoint];
+  }
+
   $scope.setPayPoint = function(point) {
     $scope.payPoint = point;
-    $scope.sortDistricts();
   };
 
-  $scope.sortDistricts();
+  // $scope.sortDistricts();
 }]);
